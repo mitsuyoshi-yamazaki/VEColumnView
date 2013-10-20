b9562695d8b46f0f8415803ec23ae290f87e35c5
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2013-10-20 22:23:17 +0900

    Initial Commit

---
+++ VEColumnViewTests/VEColumnViewTests.m
@@ -0,0 +1,34 @@
+//
+//  VEColumnViewTests.m
+//  VEColumnViewTests
+//
+//  Created by Yamazaki Mitsuyoshi on 10/20/13.
+//  Copyright (c) 2013 Mitsuyoshi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VEColumnViewTests : XCTestCase
+
+@end
+
+@implementation VEColumnViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
